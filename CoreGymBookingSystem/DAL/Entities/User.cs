cb27cb593a072ddb25db010d18e49518590e0d05
Mustafee <mussemoh03@gmail.com>
2025-11-17 13:33:27 +0100

    Added new proporties to user

--- CoreGymBookingSystem/DAL/Entities/User.cs
+++ CoreGymBookingSystem/DAL/Entities/User.cs
@@ -11,5 +11,15 @@ public class User : IdentityUser<int>
     /// <summary>
     /// All bookings made by this user.
     /// </summary>
+    
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;    
+
+  
+
     public List<Session> Bookings { get; set; } = [];
 }
