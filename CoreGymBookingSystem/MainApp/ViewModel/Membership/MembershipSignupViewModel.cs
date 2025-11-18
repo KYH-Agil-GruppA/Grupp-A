b5c5ae703bde70c6cd8d27a2e11fb995a63d313a
Nastaran Malekshahi <Nasi_Mlk@outlook.com>
2025-11-18 23:53:48 +0100

    add MembershipSignupViewModel for signup form binding

---
+++ CoreGymBookingSystem/MainApp/ViewModel/Membership/MembershipSignupViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MainApp.ViewModel.Membership
+{
+    public class MembershipSignupViewModel
+    {
+        public int MembershipTypeId { get; set; }
+        public string MembershipName { get; set; }
+
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
+
