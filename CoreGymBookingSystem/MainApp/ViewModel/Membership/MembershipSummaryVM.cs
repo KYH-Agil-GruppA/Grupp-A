b22c9130a52fdb48618fe8300bb562fd540af90c
Nastaran Malekshahi <Nasi_Mlk@outlook.com>
2025-11-18 23:55:04 +0100

    add MembershipSummaryVM for summary page data

---
+++ CoreGymBookingSystem/MainApp/ViewModel/Membership/MembershipSummaryVM.cs
@@ -0,0 +1,17 @@
+﻿namespace MainApp.ViewModel.Membership
+{
+    public class MembershipSummaryVM
+    {
+        public string MembershipName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+
+}
