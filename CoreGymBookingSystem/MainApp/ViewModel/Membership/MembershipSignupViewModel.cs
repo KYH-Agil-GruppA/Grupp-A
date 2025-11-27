6ff909c9b603fc5d85d5c3332a53b2959af39642
Nastaran Malekshahi <Nasi_Mlk@outlook.com>
2025-11-27 13:09:25 +0100

    Copied to git files

--- CoreGymBookingSystem/MainApp/ViewModel/Membership/MembershipSignupViewModel.cs
+++ CoreGymBookingSystem/MainApp/ViewModel/Membership/MembershipSignupViewModel.cs
@@ -2,5 +2,18 @@
 {
     public class MembershipSignupViewModel
     {
+        public int MembershipTypeId { get; set; }
+        public string MembershipName { get; set; }
+
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
     }
 }
+
