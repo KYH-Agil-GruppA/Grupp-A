d6ec365fb8ad0932c15c469a3e3042b931866509
Nastaran Malekshahi <Nasi_Mlk@outlook.com>
2025-11-27 13:10:15 +0100

    Copied and pushed

--- CoreGymBookingSystem/MainApp/ViewModel/Membership/MembershipSummaryVM.cs
+++ CoreGymBookingSystem/MainApp/ViewModel/Membership/MembershipSummaryVM.cs
@@ -2,5 +2,17 @@
 {
     public class MembershipSummaryVM
     {
+        public string MembershipName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int MembershipTypeId { get; set; }
     }
+
 }
