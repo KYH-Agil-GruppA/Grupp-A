2a7cfdf6526c7693055af106673c7da926050d62
acke7 <ahmadaloalbi@ouylook.com>
2025-12-03 23:28:20 +0100

    RunTime Erorr - fixed by removing migration and add new one

--- CoreGymBookingSystem/DAL/Entities/MembershipPurchase.cs
+++ CoreGymBookingSystem/DAL/Entities/MembershipPurchase.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+        using System.Text;
+        using System.Threading.Tasks;
 
-namespace DAL.Entities
-{
-    public class MembershipPurchase
-    {
-        public int Id { get; set; }
+        namespace DAL.Entities
+        {
+            public class MembershipPurchase
+            {
+                public int Id { get; set; }
 
-        public int MembershipTypeId { get; set; }
-        public MembershipType MembershipType { get; set; }
+                public int MembershipTypeId { get; set; }
+                public MembershipType MembershipType { get; set; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+                public string Email { get; set; }
+                public string Address { get; set; }
+                public string Phone { get; set; }
 
-        public DateOnly StartDate { get; set; }
-        public DateOnly PurchaseDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
-    }
-}
+                public DateOnly StartDate { get; set; }
+                public DateOnly PurchaseDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+            }
+        }
 
